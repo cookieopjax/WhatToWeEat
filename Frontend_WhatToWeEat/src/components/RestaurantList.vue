64d2e3cc46e0cc838531ec3d173317ffadba9868
cookieopjax <vigor01695@gmail.com>
2022-07-06 15:23:55 +0800

    RestaurantList card rwd

--- Frontend_WhatToWeEat/src/components/RestaurantList.vue
+++ Frontend_WhatToWeEat/src/components/RestaurantList.vue
@@ -1,34 +1,42 @@
 <script setup>
 import { reactive, onMounted } from 'vue';
-import { NCard } from 'naive-ui';
+import { NCard , NGrid, NGi } from 'naive-ui';
 import axios from 'axios';
 
 let url = 'http://localhost:4000/allRestaurant';
 let restaurantData = reactive({value:[]});
 
-
 onMounted(() => {
 	axios.get(url).then((res) => {	
 		restaurantData.value = res.data;
+		console.log(restaurantData.value);
 	}
-      
 	);
 });
 
 </script>
 
 <template>
-  <div class="flex">
-    <n-card 
-      v-for="item in restaurantData.value" 
-      :key="restaurantData.id"
-      :title="item.name" 
-			class="max-w-xs" 
-      hoverable
-    >	
-      <p>地址:{{ item.address }}</p>
-      <p>電話:{{ item.phone }}</p>
-    </n-card>
+  <div class="m-4 md:m-6 lg:m-8 xl:m-12 2xl:m-16">
+    <n-grid
+      x-gap="12"
+      y-gap="10"
+      cols="380:1 450:2 600:3 900:4"
+    >
+      <n-gi 
+        v-for="item in restaurantData.value" 
+        :key="restaurantData.id"
+        class="flex items-start"
+      >
+        <n-card 
+          :title="item.name" 
+          hoverable
+        >	
+          <p>地址:{{ item.address }}</p>
+          <p>電話:{{ item.phone }}</p>
+        </n-card>
+      </n-gi>
+    </n-grid>
   </div>
 </template>
 
