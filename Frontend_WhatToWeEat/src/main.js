33c2c548f41c5fe5ba045150a6dde845a8b44a93
cookieopjax <vigor01695@gmail.com>
2022-10-06 19:15:44 +0800

    setup pinia

--- Frontend_WhatToWeEat/src/main.js
+++ Frontend_WhatToWeEat/src/main.js
@@ -4,13 +4,16 @@ import App from './App.vue';
 import './index.css';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import { createPinia } from 'pinia';
+
+
 
 
 const app = createApp(App);
 
 app.use(router);
 app.use(VueAxios, axios);
-
+app.use(createPinia());
 
 
 app.mount('#app');
