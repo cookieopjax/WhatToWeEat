e98834698234a80375525ee2e5349b1f95192656
cookie_opjax <vigor01695@gmail.com>
2023-03-23 16:56:22 +0800

    Backend CI Setup

--- Backend_WhatToWeEat/app.js
+++ Backend_WhatToWeEat/app.js
@@ -5,7 +5,7 @@ const swaggerUi = require("swagger-ui-express"); // 產生swagger ui介面
 const { errorHandler } = require("./middlewares/errorHandler");
 const userRoutes = require("./routes/userRoutes"); // 引入userRoutes.js路由文件
 const restaurantRoutes = require("./routes/restaurantRoutes"); // 引入userRoutes.js路由文件
-const { sequelize, swaggerSpec } = require("./utils/database");
+const { swaggerSpec } = require("./utils/database");
 
 if (process.env.NODE_ENV === "production") {
   dotenv.config({ path: ".env" });
@@ -17,9 +17,6 @@ const app = express();
 
 // 暫時開發用，允許全部
 app.use(cors());
-
-// 進行資料庫更新
-// sequelize.sync();
 
 app.use(express.json());
 
@@ -36,9 +33,3 @@ app.use(errorHandler);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 module.exports = app;
-
-// 啟動Express
-// app.listen(4234, () => {
-//   console.log(`-> Express server listening on port http://localhost:4234
-// -> You can see the api docs on http://localhost:4234/api-docs`);
-// });
